955296c62bb7d6bfd477128b164727edb436f62f
chandananandan09 <131899503+chandananandan09@users.noreply.github.com>
2023-05-02 03:21:37 +0000

    Intial Changes

--- src/components/GradeTable.tsx
+++ src/components/GradeTable.tsx
@@ -1,21 +1,64 @@
-/**
- * You might find it useful to have some dummy data for your own testing.
- * Feel free to write this function if you find that feature desirable.
- * 
- * When you come to office hours for help, we will ask you if you have written
- * this function and tested your project using it.
- */
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+// import {useEffect, useState } from 'react';
+
+// useEffect(() => {
+  
+// })
+
+
 export function dummyData() {
   return [];
 }
 
-/**
- * This is the component where you should write the code for displaying the
- * the table of grades.
- *
- * You might need to change the signature of this function.
- *
- */
+
 export const GradeTable = () => {
-  return <></>;
+  let headers =  {
+    'buid' : 'U14131622',
+    'x-functions-key' : 'SHSHFt8CN3CxKEUGRrPt0hQohqQ3YDWF0DNT0UaQqIALAzFuCzfNfw=='
+  };
+  fetch("https://spark-se-assessment-api.azurewebsites.net/api/student/findByStatus/enrolled",headers)
+  .then((response) => (response.json))
+  .then(data => {
+    console.log(data)
+  })
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+  return <>
+    <DataGrid
+  rows={rows}
+  columns={columns}
+  // paginationModel={{ page: 0, pageSize: 5 }}
+  checkboxSelection
+/>
+  </>;
 };
